883bdd2c6485393da8fa637d235cb6cce1b83278
voevodin.kirill <voev.kirill@gmail.com>
2020-12-27 22:44:53 +0300

    some fixes

--- include/gostone/keymgmt/keymgmt_impl.h
+++ include/gostone/keymgmt/keymgmt_impl.h
@@ -1,8 +1,13 @@
 #pragma once
 #include <openssl/core_dispatch.h>
 
-/* Key management (base) */
+/* Algorithm specific */
 OSSL_FUNC_keymgmt_new_fn GsKeyMgmtNew;
+OSSL_FUNC_keymgmt_new_fn GsKeyMgmtNew512;
+OSSL_FUNC_keymgmt_gen_init_fn GsKeyMgmtGenInit;
+OSSL_FUNC_keymgmt_gen_init_fn GsKeyMgmtGenInit512;
+
+/* Key management (base) */
 OSSL_FUNC_keymgmt_free_fn GsKeyMgmtFree;
 OSSL_FUNC_keymgmt_load_fn GsKeyMgmtLoad;
 OSSL_FUNC_keymgmt_get_params_fn GsKeyMgmtGetParams;
@@ -23,7 +28,6 @@ OSSL_FUNC_keymgmt_export_fn GsKeyMgmtExport;
 OSSL_FUNC_keymgmt_export_types_fn GsKeyMgmtExportTypes;
 
 /* Key management (generation) */
-OSSL_FUNC_keymgmt_gen_init_fn GsKeyMgmtGenInit;
 OSSL_FUNC_keymgmt_gen_set_template_fn GsKeyMgmtGenSetTemplate;
 OSSL_FUNC_keymgmt_gen_set_params_fn GsKeyMgmtGenSetParams;
 OSSL_FUNC_keymgmt_gen_settable_params_fn GsKeyMgmtGenSettableParams;
