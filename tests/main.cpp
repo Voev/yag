34b8e98162f5db930c1c70df9f9197c842ce59d3
voevodin.kirill <voev.kirill@gmail.com>
2021-06-26 17:55:49 +0300

    style refactoring

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,16 +1,15 @@
 #include <gtest/gtest.h>
-#include <openssl/provider.h>
 #include <openssl/err.h>
+#include <openssl/provider.h>
 
-int main( int argc, char* argv[] )
+int main(int argc, char* argv[])
 {
-    testing::InitGoogleTest( &argc, argv );
-    OSSL_PROVIDER* defaultProv = OSSL_PROVIDER_load( nullptr, "default" );
-    OSSL_PROVIDER* prov = OSSL_PROVIDER_load( nullptr, "gostone" );
-    ERR_print_errors_fp( stderr );
+    testing::InitGoogleTest(&argc, argv);
+    OSSL_PROVIDER* defaultProv = OSSL_PROVIDER_load(nullptr, "default");
+    OSSL_PROVIDER* prov = OSSL_PROVIDER_load(nullptr, "gostone");
+    ERR_print_errors_fp(stderr);
     int ret = RUN_ALL_TESTS();
-    OSSL_PROVIDER_unload( prov );
-    OSSL_PROVIDER_unload( defaultProv );
+    OSSL_PROVIDER_unload(prov);
+    OSSL_PROVIDER_unload(defaultProv);
     return ret;
 }
-
