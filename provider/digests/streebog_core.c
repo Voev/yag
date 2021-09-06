3731df1d5dd608d2821ead78dbd295502c42eca6
voevodin.kirill <voev.kirill@gmail.com>
2021-09-06 23:49:19 +0300

    fix digest algorithm

--- provider/digests/streebog_core.c
+++ provider/digests/streebog_core.c
@@ -4,111 +4,112 @@
 
 struct gs_streebog_ctx_st
 {
-    uint64_t h[ BlockLengthInUInt64 ];
-    uint64_t N[ BlockLengthInUInt64 ];
-    uint64_t sigma[ BlockLengthInUInt64 ];
-    uint64_t buffer[ BlockLengthInUInt64 ];
-    uint64_t digest[ BlockLengthInUInt64 ];
-    size_t   bufferSize;
-    size_t   digestSize;
+    union
+    {
+        uint8_t u8[BlockLengthInBytes];
+        uint64_t u64[BlockLengthInUInt64];
+    } buffer;
+
+    uint64_t h[BlockLengthInUInt64];
+    uint64_t N[BlockLengthInUInt64];
+    uint64_t sigma[BlockLengthInUInt64];
+    uint64_t digest[BlockLengthInUInt64];
+    size_t bufferSize;
+    size_t digestSize;
 };
 
-static inline
-void doPadding( uint64_t buffer[], size_t bufferSize )
+static inline void doPadding(uint64_t buffer[], size_t bufferSize)
 {
-    size_t appendIndex = bufferSize / sizeof( uint64_t );
-    size_t appendPosition = bufferSize % sizeof( uint64_t );
+    size_t appendIndex = bufferSize / sizeof(uint64_t);
+    size_t appendPosition = bufferSize % sizeof(uint64_t);
 
-    for( size_t i = appendIndex + 1; i < BlockLengthInUInt64; ++i )
+    for (size_t i = appendIndex + 1; i < BlockLengthInUInt64; ++i)
     {
-         buffer[ i ] = 0;
+        buffer[i] = 0;
     }
-    uint64_t appendBit = UINT64_C( 1 ) << ( 8 * appendPosition );
-    buffer[ appendIndex ] &= appendBit - 1;
-    buffer[ appendIndex ] |= appendBit;
+    uint64_t appendBit = UINT64_C(1) << (8 * appendPosition);
+    buffer[appendIndex] &= appendBit - 1;
+    buffer[appendIndex] |= appendBit;
 }
 
-static inline
-void doAdd( uint64_t r[], const uint64_t x[], const uint64_t y[] )
+static inline void doAdd(uint64_t r[], const uint64_t x[], const uint64_t y[])
 {
-    for( uint8_t carry = 0, i = 0; i < BlockLengthInUInt64; ++i )
+    for (uint8_t carry = 0, i = 0; i < BlockLengthInUInt64; ++i)
     {
-        const uint64_t left = x[ i ];
+        const uint64_t left = x[i];
         uint64_t sum;
 
-        sum = left + y[ i ] + carry;
-        if( sum != left )
+        sum = left + y[i] + carry;
+        if (sum != left)
         {
-            carry = ( sum < left );
-            r[ i ] = sum;
+            carry = (sum < left);
+            r[i] = sum;
         }
     }
 }
 
-static inline
-void doXor( uint64_t r[], const uint64_t x[], const uint64_t y[] )
+static inline void doXor(uint64_t r[], const uint64_t x[], const uint64_t y[])
 {
-    for( size_t i = 0; i < BlockLengthInUInt64; ++i )
+    for (size_t i = 0; i < BlockLengthInUInt64; ++i)
     {
-        r[ i ] = x[ i ] ^ y[ i ];
+        r[i] = x[i] ^ y[i];
     }
 }
 
-static inline
-void doLPS( uint64_t r[], const uint64_t x[], const uint64_t y[] )
+static inline void doLPS(uint64_t r[], const uint64_t x[], const uint64_t y[])
 {
-    uint64_t a[ BlockLengthInUInt64 ] = { 0 };
-    doXor( a, x, y );
+    uint64_t a[BlockLengthInUInt64] = {0};
+    doXor(a, x, y);
 
-    for( size_t i = 0; i < BlockLengthInUInt64; ++i )
+    for (size_t i = 0; i < BlockLengthInUInt64; ++i)
     {
-        r[ i ]  = Ax[ 0 ][ ( a[ 0 ] >> ( i << 3 ) ) & 0xFF ];
-        r[ i ] ^= Ax[ 1 ][ ( a[ 1 ] >> ( i << 3 ) ) & 0xFF ];
-        r[ i ] ^= Ax[ 2 ][ ( a[ 2 ] >> ( i << 3 ) ) & 0xFF ];
-        r[ i ] ^= Ax[ 3 ][ ( a[ 3 ] >> ( i << 3 ) ) & 0xFF ];
-        r[ i ] ^= Ax[ 4 ][ ( a[ 4 ] >> ( i << 3 ) ) & 0xFF ];
-        r[ i ] ^= Ax[ 5 ][ ( a[ 5 ] >> ( i << 3 ) ) & 0xFF ];
-        r[ i ] ^= Ax[ 6 ][ ( a[ 6 ] >> ( i << 3 ) ) & 0xFF ];
-        r[ i ] ^= Ax[ 7 ][ ( a[ 7 ] >> ( i << 3 ) ) & 0xFF ];
+        r[i] = Ax[0][(a[0] >> (i << 3)) & 0xFF];
+        r[i] ^= Ax[1][(a[1] >> (i << 3)) & 0xFF];
+        r[i] ^= Ax[2][(a[2] >> (i << 3)) & 0xFF];
+        r[i] ^= Ax[3][(a[3] >> (i << 3)) & 0xFF];
+        r[i] ^= Ax[4][(a[4] >> (i << 3)) & 0xFF];
+        r[i] ^= Ax[5][(a[5] >> (i << 3)) & 0xFF];
+        r[i] ^= Ax[6][(a[6] >> (i << 3)) & 0xFF];
+        r[i] ^= Ax[7][(a[7] >> (i << 3)) & 0xFF];
     }
 }
 
-void g( uint64_t h[], const uint64_t N[], const uint64_t m[] )
+void g(uint64_t h[], const uint64_t N[], const uint64_t m[])
 {
-    uint64_t K [ BlockLengthInUInt64 ] = { 0 };
-    uint64_t Ki[ BlockLengthInUInt64 ] = { 0 };
+    uint64_t K[BlockLengthInUInt64] = {0};
+    uint64_t Ki[BlockLengthInUInt64] = {0};
 
     /*
      * $K := LPS(h \xor N)
      */
-    doLPS( K, h, N );
+    doLPS(K, h, N);
 
     /*
      * E(K, m) := X[K_{13}]LPSX[K_{12}]...LPSX[K_1](m)
      * K_1 := K
      * $K := LPSX[K_1](m)
      */
-    memcpy( Ki, K, BlockLengthInBytes );
-    doLPS( K, Ki, m );
+    memcpy(Ki, K, BlockLengthInBytes);
+    doLPS(K, Ki, m);
 
-    for( int i = 0; i < NumberOfRounds - 1; ++i )
+    for (int i = 0; i < NumberOfRounds - 1; ++i)
     {
         /* K_2 := LPS(K_1 \xor C_1) etc. */
-        doLPS( Ki, Ki, &C[ i * BlockLengthInUInt64 ] );
+        doLPS(Ki, Ki, &C[i * BlockLengthInUInt64]);
         /* $K := LPSX[K_2]LPSX[K_1](m) etc. */
-        doLPS( K, Ki, K );
+        doLPS(K, Ki, K);
     }
 
     /* Last round */
-    doLPS( Ki, Ki, &C[ ( NumberOfRounds - 1 ) * BlockLengthInUInt64 ] );
-    doXor( K, Ki, K );
+    doLPS(Ki, Ki, &C[(NumberOfRounds - 1) * BlockLengthInUInt64]);
+    doXor(K, Ki, K);
 
     /* E(K, m) done */
-    doXor( K, K, h );
-    doXor( h, K, m );
+    doXor(K, K, h);
+    doXor(h, K, m);
 }
 
-void doStage2( GsStreebogCtx* ctx, const void* data )
+void doStage2(GsStreebogCtx* ctx, const void* data)
 {
     /*
      * M := M' || m, m \in V_{512} (last 512 bits of $data)
@@ -117,114 +118,108 @@ void doStage2( GsStreebogCtx* ctx, const void* data )
      * sigma := Vec_{512} ( Int_{512}(sigma) \boxplus  Int_{512}(m) )
      * M := M'
      */
-    uint64_t m[ BlockLengthInUInt64 ] = { 0 };
-    memcpy( m, data, sizeof( m ) );
+    uint64_t m[BlockLengthInUInt64] = {0};
+    memcpy(m, data, sizeof(m));
 
-    g( ctx->h, ctx->N, m );
-    doAdd( ctx->N, ctx->N, gConst512 );
-    doAdd( ctx->sigma, ctx->sigma, m );
+    g(ctx->h, ctx->N, m);
+    doAdd(ctx->N, ctx->N, gConst512);
+    doAdd(ctx->sigma, ctx->sigma, m);
 }
 
-void doStage3( GsStreebogCtx* ctx )
+void doStage3(GsStreebogCtx* ctx)
 {
-    uint64_t buffer[ BlockLengthInUInt64 ] = { 0 };
-    memcpy( buffer, ctx->buffer, sizeof( buffer ) );
+    uint64_t buffer[BlockLengthInUInt64] = {0};
+    memcpy(buffer, ctx->buffer.u64, sizeof(buffer));
 
-    memcpy( ctx->buffer, buffer, sizeof( buffer ) );
-    memset( buffer, 0x00, sizeof( buffer ) );
+    memcpy(ctx->buffer.u64, buffer, sizeof(buffer));
+    memset(buffer, 0x00, sizeof(buffer));
 
-    buffer[ 0 ] = ctx->bufferSize << 3;
+    buffer[0] = ctx->bufferSize << 3;
 
-    doPadding( ctx->buffer, ctx->bufferSize );
+    doPadding(ctx->buffer.u64, ctx->bufferSize);
 
-    g( ctx->h, ctx->N, ctx->buffer );
+    g(ctx->h, ctx->N, ctx->buffer.u64);
 
     /*
      * N     := Vec_512(Int_512(N) \boxplus |M|)
      * Sigma := Vec_512(Int_512(Sigma) \boxplus Int_512(m))
      */
-    doAdd( ctx->N, ctx->N, buffer );
-    doAdd( ctx->sigma, ctx->sigma, ctx->buffer );
+    doAdd(ctx->N, ctx->N, buffer);
+    doAdd(ctx->sigma, ctx->sigma, ctx->buffer.u64);
 
-    g( ctx->h, gConst0, ctx->N );
-    g( ctx->h, gConst0, ctx->sigma );
+    g(ctx->h, gConst0, ctx->N);
+    g(ctx->h, gConst0, ctx->sigma);
 
-    memcpy( ctx->digest, ctx->h, sizeof( ctx->h ) );
+    memcpy(ctx->digest, ctx->h, sizeof(ctx->h));
 }
 
-void GsStreebogCtxInit( GsStreebogCtx* ctx )
+void GsStreebogCtxInit(GsStreebogCtx* ctx)
 {
     /* Stage 1:
      *    h = IV
      *    N = 0^{512} \in V_{512}
      *    sigma = 0^{512} \in V_{512}
      */
-    if( ctx->digestSize == Streebog256LengthInBytes )
+    if (ctx->digestSize == Streebog256LengthInBytes)
     {
-        memset( ctx->h, 0x01, BlockLengthInBytes );
+        memset(ctx->h, 0x01, BlockLengthInBytes);
     }
 }
 
-void GsStreebogCtxUpdate( GsStreebogCtx* ctx, const uint8_t* data, size_t dataSize )
+void GsStreebogCtxUpdate(GsStreebogCtx* ctx, const uint8_t* data,
+                         size_t dataSize)
 {
     size_t chunkSize;
 
     /* Stage 2: do until |M| < 512 bits, M = $data */
-    while( BlockLengthInBytes < dataSize && !ctx->bufferSize )
+    while (BlockLengthInBytes < dataSize && !ctx->bufferSize)
     {
-        doStage2( ctx, data );
+        doStage2(ctx, data);
 
-        data     += BlockLengthInBytes;
+        data += BlockLengthInBytes;
         dataSize -= BlockLengthInBytes;
     }
 
-    while( dataSize )
+    while (dataSize)
     {
         chunkSize = BlockLengthInBytes - ctx->bufferSize;
-        if( chunkSize > dataSize )
+        if (chunkSize > dataSize)
         {
             chunkSize = dataSize;
         }
 
-        for( size_t i = 0; i < BlockLengthInUInt64; ++i )
-        {
-            uint64_t v = 0;
-            memcpy( &v, &data[ i * sizeof( uint64_t ) ], sizeof( uint64_t ) );
-            ctx->buffer[ i ] = v;//bswap_64( v );
-        }
-
+        memcpy(&ctx->buffer.u8[ctx->bufferSize], data, chunkSize);
         ctx->bufferSize += chunkSize;
 
-        data     += chunkSize;
+        data += chunkSize;
         dataSize -= chunkSize;
 
-        if( BlockLengthInBytes == ctx->bufferSize )
+        if (BlockLengthInBytes == ctx->bufferSize)
         {
-            doStage2( ctx, ctx->buffer );
+            doStage2(ctx, ctx->buffer.u64);
             ctx->bufferSize = 0;
         }
     }
 }
 
-void GsStreebogCtxFinal( GsStreebogCtx* ctx, uint8_t* digest )
+void GsStreebogCtxFinal(GsStreebogCtx* ctx, uint8_t* digest)
 {
-    doStage3( ctx );
+    doStage3(ctx);
     ctx->bufferSize = 0;
 
-    if( ctx->digestSize == Streebog256LengthInBytes )
+    if (ctx->digestSize == Streebog256LengthInBytes)
     {
-        memcpy( digest, &ctx->digest[ BlockLengthInUInt64 / 2 ],
-                ctx->digestSize );
+        memcpy(digest, &ctx->digest[BlockLengthInUInt64 / 2], ctx->digestSize);
     }
     else
     {
-        memcpy( digest, ctx->digest, ctx->digestSize );
+        memcpy(digest, ctx->digest, ctx->digestSize);
     }
 }
 
-int GsStreebogCtxSetDgstSize( GsStreebogCtx* ctx, const size_t digestSize )
+int GsStreebogCtxSetDgstSize(GsStreebogCtx* ctx, const size_t digestSize)
 {
-    if( ctx )
+    if (ctx)
     {
         ctx->digestSize = digestSize;
         return 1;
@@ -232,12 +227,12 @@ int GsStreebogCtxSetDgstSize( GsStreebogCtx* ctx, const size_t digestSize )
     return 0;
 }
 
-size_t GsStreebogCtxGetDgstSize( GsStreebogCtx* ctx )
+size_t GsStreebogCtxGetDgstSize(GsStreebogCtx* ctx)
 {
     return ctx ? ctx->digestSize : 0;
 }
 
-size_t GsStreebogCtxGetSize( void )
+size_t GsStreebogCtxGetSize(void)
 {
-    return sizeof( GsStreebogCtx );
+    return sizeof(GsStreebogCtx);
 }
